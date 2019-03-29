cd12ee233a6ad4254c46a2011a9d99fe0adcde3f
Zozan CAN <zzncan@gmail.com>
2019-03-29 16:18:27 +0300

    Control flow statements branch added.

--- src/ControlFlowStatements/Exceptions.java
+++ src/ControlFlowStatements/Exceptions.java
@@ -3,7 +3,6 @@ package ControlFlowStatements;
 public class Exceptions {
     public static void main(String[] args) {
 
-
         // int[] myNumbers = {1, 2, 3};
         //System.out.println(myNumbers[8]); // error!
         //If an error occurs, we can use try...catch to catch the error and execute some code to handle it:
