2da4e790e4d37833103cdbaceace1a115c238b62
Zozan CAN <zzncan@gmail.com>
2019-03-29 14:59:02 +0300

    Comments added.

---
+++ src/DataTypesAndVariables/Comments.java
@@ -0,0 +1,22 @@
+package DataTypesAndVariables;
+
+public class Comments {
+
+    public static void main(String[] args) {
+
+        // to comment on a single line.
+
+
+/*
+  to comment on multiple lines
+ */
+
+
+/**
+ *
+ * This is the type of comment that we use to make parameter and class definitions.
+ * You can use for annotation such as @param in this line
+ *
+ */
+    }
+}
