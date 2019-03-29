3de6f3998f1e0e2882e1bdd4d6c6d27c19f8638c
Zozan CAN <zzncan@gmail.com>
2019-03-29 16:28:53 +0300

    While loop added.

---
+++ src/LoopControlStatements/WhileLoop.java
@@ -0,0 +1,23 @@
+package LoopControlStatements;
+
+public class WhileLoop {
+
+    public static void main(String[] args) {
+
+        int y = 0;
+        while (y < 5) {
+            System.out.println(y);
+            y++;
+        }
+
+        int z = 0;
+        do {
+            System.out.println(z);
+            z++;
+        }
+        while (z < 5);
+
+
+
+    }
+}
