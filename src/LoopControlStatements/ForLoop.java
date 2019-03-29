25faecfceb4468acd140daeac68a76bcc7e28976
Zozan CAN <zzncan@gmail.com>
2019-03-29 16:41:12 +0300

    For loop added.

---
+++ src/LoopControlStatements/ForLoop.java
@@ -0,0 +1,11 @@
+package LoopControlStatements;
+
+public class ForLoop {
+
+    public static void main(String[] args) {
+        for (int x =0; x<10; x++) {
+            System.out.println("Captain America!" +x);
+
+        }
+    }
+}
