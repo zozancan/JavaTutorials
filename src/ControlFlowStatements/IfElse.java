25a904b8e0e4f895b2ac65c3099b06141a82d48b
Zozan CAN <zzncan@gmail.com>
2019-03-29 15:15:55 +0300

    If else expression added.

---
+++ src/ControlFlowStatements/IfElse.java
@@ -0,0 +1,15 @@
+package ControlFlowStatements;
+
+public class IfElse {
+
+    public static void main(String[] args) {
+
+        int number = -37;
+
+        if (number>0) {
+            System.out.println("Positive number!");
+        } else {
+            System.out.println("Negative number!");
+        }
+    }
+}
