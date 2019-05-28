45ec92b1b6becbab6ea6ced1a4a4e5c51dcbf790
Zozan Can <zzncan@gmail.com>
2019-05-28 11:03:59 +0300

    IfElse example added

--- src/ControlFlowStatements/IfElse.java
+++ src/ControlFlowStatements/IfElse.java
@@ -35,5 +35,23 @@ public class IfElse {
             System.out.println("Awesome!");
         }
 
+        //Fourth example
+
+        int month = 4;
+        String season;
+
+        if (month == 12 || month == 1 || month == 2)
+            season = "Winter";
+        else if (month == 3 || month == 4 || month == 5)
+            season = "Spring";
+        else if (month == 6 || month == 7 || month == 8)
+            season = "Summer";
+        else if (month == 9 || month == 10 || month == 11)
+            season = "Autumn";
+        else
+            season = "Bogus Month";
+
+        System.out.println("April is the " + season + ".");
+
     }
 }
