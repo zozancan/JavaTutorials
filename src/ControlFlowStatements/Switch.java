9c0d6e39acf702bff4f095c746cbeed69fa4f81f
Zozan Can <zzncan@gmail.com>
2019-05-28 11:12:00 +0300

    Switch example added

--- src/ControlFlowStatements/Switch.java
+++ src/ControlFlowStatements/Switch.java
@@ -3,6 +3,8 @@ package ControlFlowStatements;
 public class Switch {
     public static void main(String[] args) {
 
+        //example
+
          int day =3;
          switch (day) {
              case 1:
@@ -27,5 +29,26 @@ public class Switch {
                  System.out.println("Sunday!");
                  break;
          }
+
+        //example
+
+        for (int i=0; i<6; i++) {
+            switch (i) {
+                case 0:
+                    System.out.println("i is zero.");
+                    break;
+                case 1:
+                    System.out.println("i is one.");
+                    break;
+                case 2:
+                    System.out.println("i is two");
+                    break;
+                case 3:
+                    System.out.println("i is three.");
+                    break;
+                    default:
+                        System.out.println("i is greater than 3.");
+            }
+        }
     }
 }
