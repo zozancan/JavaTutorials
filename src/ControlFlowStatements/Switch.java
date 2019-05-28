69c8f55c7ba314008cd27b08289fdbc0445a157d
Zozan Can <zzncan@gmail.com>
2019-05-28 11:23:40 +0300

    Switch example added++

--- src/ControlFlowStatements/Switch.java
+++ src/ControlFlowStatements/Switch.java
@@ -50,5 +50,37 @@ public class Switch {
                         System.out.println("i is greater than 3.");
             }
         }
+
+
+        //example
+
+        int month = 4;
+        String season;
+
+        switch (month) {
+            case 12:
+            case 1:
+            case 2:
+                season = "Winter";
+                break;
+            case 3:
+            case 4:
+            case 5:
+                season = "Spring";
+                break;
+            case 6:
+            case 7:
+            case 8:
+                season = "Summer";
+                break;
+            case 9:
+            case 10:
+            case 11:
+                season = "Autumn";
+                break;
+                default:
+                    season = "Bogus Month";
+        }
+        System.out.println("April is in the " + season + ".");
     }
 }
